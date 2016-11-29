e524195537072706fc6175fa4bcf00a45c5d0c7b
hyoklee <hyoklee@alpaca.ad.hdfgroup.org>
2016-11-29 08:36:33 -0600

    EED-118:turned off local testing. updated comment to refer GitHub wiki page.

--- shell/use-cases/test_1.sh
+++ shell/use-cases/test_1.sh
@@ -2,8 +2,7 @@
 # Copyright (C) 2016 The HDF Group
 # All rights reserved.
 #
-# This shell script measures the performance of Hyrax server that runs on
-# Amazon Web Service (EC2/S3).
+# https://github.com/OPENDAP/cloudydap/wiki/Trade-Study-Use-Case-1---DAP2-with-CF
 #
 echo 'Test AIRS DAS.'
 time curl -s http://52.55.197.86:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.das -o airs.das
@@ -18,18 +17,18 @@ time curl -s http://52.55.197.86:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.R
 echo 'Test AIRS DODS subsetting.'
 time curl -s -g http://52.55.197.86:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dods\?Topography\[0:20:179\]\[0:20:359\] -o airs_d2_ss.bin
 
-echo 'Test AIRS DAS (local).'
-time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.das -o airs_local.das
+# echo 'Test AIRS DAS (local).'
+# time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.das -o airs_local.das
 
-echo 'Test AIRS DDS (local).'
-time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dds -o airs_local.dds
+# echo 'Test AIRS DDS (local).'
+# time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dds -o airs_local.dds
 
-echo 'Test AIRS DODS (local).'
-time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dods\?Latitude,Longitude,Topography -o airs_local_d2_ll.bin
+# echo 'Test AIRS DODS (local).'
+# time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dods\?Latitude,Longitude,Topography -o airs_local_d2_ll.bin
 
 
-echo 'Test AIRS DODS subsetting (local).'
-time curl -s -g http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dods\?Topography\[0:20:179\]\[0:20:359\] -o airs_local_d2_ss.bin
+# echo 'Test AIRS DODS subsetting (local).'
+# time curl -s -g http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dods\?Topography\[0:20:179\]\[0:20:359\] -o airs_local_d2_ss.bin
 
 diff airs_local.das airs.das
 diff airs_local.dds airs.dds
--- shell/use-cases/test_2.sh
+++ shell/use-cases/test_2.sh
@@ -13,14 +13,14 @@ time curl -s http://52.55.197.86:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.R
 echo 'Test AIRS DAP4 subsetting.'
 time curl -s -g http://52.55.197.86:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\] -o airs_d4_ss.bin
 
-echo 'Test AIRS DMR (local).'
-time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmr -o airs_local.dmr
+# echo 'Test AIRS DMR (local).'
+# time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmr -o airs_local.dmr
 
-echo 'Test AIRS DAP4 (local).'
-time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dap\?dap4.ce=Latitude\;Longitude\;Topography -o airs_local_d4_ll.bin
+# echo 'Test AIRS DAP4 (local).'
+# time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dap\?dap4.ce=Latitude\;Longitude\;Topography -o airs_local_d4_ll.bin
 
-echo 'Test AIRS DAP4 subsetting (local).'
-time curl -s -g http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\] -o airs_local_d4_ss.bin
+# echo 'Test AIRS DAP4 subsetting (local).'
+# time curl -s -g http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\] -o airs_local_d4_ss.bin
 
 # There should be only URL name difference:
 # 
