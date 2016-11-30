dec78956254a7425832f103f208f873582009a6e
H. Joe Lee <hyoklee@hdfgroup.org>
2016-11-30 13:26:52 -0600

    EED-102:added test script for use case 6 with expected output.

---
+++ shell/use-cases/test_7.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (C) 2016 The HDF Group
+# All rights reserved.
+#
+# MERRA aggregation test.
+# https://github.com/OPENDAP/cloudydap/wiki/Trade-Study-Use-Case-6-----Simulate-NcML-JoinExisting-aggregation
+curl https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/cloudydap/merra2.tar.gz
+
