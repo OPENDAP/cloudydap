0f78e07740807f58933e6127994917d30bb325df
hyoklee <hyoklee@alpaca.ad.hdfgroup.org>
2016-12-20 08:53:34 -0600

    EED-131:added expected output. test passed for architecture 1.

--- shell/use-cases/test_9.sh
+++ shell/use-cases/test_9.sh
@@ -24,11 +24,11 @@ time curl -s -g http://52.55.197.86:8080/s3/dap/cloudydap/samples/GSSTFYC.3.Year
 # time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/samples/GSSTFYC.3.Year.1988_2008.he5.dds -o gsstf_local.dds
 
 # echo 'Test GSSTF DODS (local).'
-# time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/samples/GSSTFYC.3.Year.1988_2008.he5.dods\?Latitude,Longitude,Topography -o gsstf_local_d2_ll.bin
+3 time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/samples/GSSTFYC.3.Year.1988_2008.he5.dods\?lon,lat,NCEP_SST -o gsstf_local_d2_ll.bin
 
 
 # echo 'Test GSSTF DODS subsetting (local).'
-# time curl -s -g http://localhost:8081/opendap/s3/dap/cloudydap/samples/GSSTFYC.3.Year.1988_2008.he5.dods\?Topography\[0:20:179\]\[0:20:359\] -o gsstf_local_d2_ss.bin
+# time curl -s -g http://localhost:8081/opendap/s3/dap/cloudydap/samples/GSSTFYC.3.Year.1988_2008.he5.dods\?lat\[0:100:719\],lon\[0:100:1339\],NCEP_SST\[0:100:719\]\[0:100:1339\] -o gsstf_local_d2_ss.bin
 
 diff gsstf_local.das gsstf.das
 diff gsstf_local.dds gsstf.dds
