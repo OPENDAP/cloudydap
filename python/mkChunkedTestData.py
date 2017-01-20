553fbb362256fa4fd0de8409a44e2313dc4708e1
ndp-opendap <ndp@opendap.org>
2017-01-20 10:48:21 -0800

    Added more test files.

--- python/mkChunkedTestData.py
+++ python/mkChunkedTestData.py
@@ -144,3 +144,92 @@ dset = f.create_dataset('d_16_gzipped_chunks', (40,40,40,40), maxshape=(40,40,40
 # initialize dataset values here
 f.close()
 
+#########################################################################
+# Make HDF5 test files where the dataset/variable is both chunked 
+# and shuffled.
+
+
+# creating file: chunked_shuffled_oneD.h5
+f = h5py.File('chunked_shuffled_oneD.h5', 'w')
+# Dataset: /d_4_shuffled_chunks
+dt = np.dtype('<f4')
+arr = np.arange(40000, dtype=dt)
+dset = f.create_dataset('d_4_shuffled_chunks', (40000,), maxshape=(40000,), chunks=(10000,), shuffle=True, dtype=dt, data=arr)
+# initialize dataset values here
+f.close()
+
+
+# creating file: chunked_gziped_twoD.h5
+f = h5py.File('chunked_shuffled_twoD.h5', 'w')
+# Dataset: /d_4_shuffled_chunks
+dt = np.dtype('<f4')
+arr2 = np.arange(10000, dtype=dt).reshape(100,100)
+dset = f.create_dataset('d_4_shuffled_chunks', (100,100), maxshape=(100,100), chunks=(50,50), shuffle=True, dtype=dt, data=arr2)
+# initialize dataset values here
+f.close()
+
+# creating file: chunked_shuffled_threeD.h5
+f = h5py.File('chunked_shuffled_threeD.h5', 'w')
+# Dataset: /d_8_shuffled_chunks
+dt = np.dtype('<f4')
+arr3 = np.arange(1000000, dtype=dt).reshape(100,100,100)
+dset = f.create_dataset('d_8_shuffled_chunks', (100,100,100), maxshape=(100,100,100), shuffle=True, dtype=dt, data=arr3)
+# initialize dataset values here
+f.close()
+
+
+# creating file: chunked_shuffled_fourD.h5
+f = h5py.File('chunked_shuffled_fourD.h5', 'w')
+# Dataset: /d_16_shuffled_chunks
+dt = np.dtype('<f4')
+arr4 = np.arange(2560000, dtype=dt).reshape(40,40,40,40)
+dset = f.create_dataset('d_16_shuffled_chunks', (40,40,40,40), maxshape=(40,40,40,40), shuffle=True, dtype=dt, data=arr4)
+# initialize dataset values here
+f.close()
+
+
+
+#########################################################################
+# Make HDF5 test files where the dataset/variable is chunked, shuffled,
+# and compressed.
+
+
+# creating file: chunked_shufzip_oneD.h5
+f = h5py.File('chunked_shufzip_oneD.h5', 'w')
+# Dataset: /d_4_shufzip_chunks
+dt = np.dtype('<f4')
+arr = np.arange(40000, dtype=dt)
+dset = f.create_dataset('d_4_shufzip_chunks', (40000,), maxshape=(40000,), chunks=(10000,), compression='gzip', compression_opts=6, shuffle=True, dtype=dt, data=arr)
+# initialize dataset values here
+f.close()
+
+
+# creating file: chunked_gziped_twoD.h5
+f = h5py.File('chunked_shufzip_twoD.h5', 'w')
+# Dataset: /d_4_shufzip_chunks
+dt = np.dtype('<f4')
+arr2 = np.arange(10000, dtype=dt).reshape(100,100)
+dset = f.create_dataset('d_4_shufzip_chunks', (100,100), maxshape=(100,100), chunks=(50,50), compression='gzip', compression_opts=6, shuffle=True, dtype=dt, data=arr2)
+# initialize dataset values here
+f.close()
+
+# creating file: chunked_shufzip_threeD.h5
+f = h5py.File('chunked_shufzip_threeD.h5', 'w')
+# Dataset: /d_8_shufzip_chunks
+dt = np.dtype('<f4')
+arr3 = np.arange(1000000, dtype=dt).reshape(100,100,100)
+dset = f.create_dataset('d_8_shufzip_chunks', (100,100,100), maxshape=(100,100,100), compression='gzip', compression_opts=6, shuffle=True, dtype=dt, data=arr3)
+# initialize dataset values here
+f.close()
+
+
+# creating file: chunked_shufzip_fourD.h5
+f = h5py.File('chunked_shufzip_fourD.h5', 'w')
+# Dataset: /d_16_shufzip_chunks
+dt = np.dtype('<f4')
+arr4 = np.arange(2560000, dtype=dt).reshape(40,40,40,40)
+dset = f.create_dataset('d_16_shufzip_chunks', (40,40,40,40), maxshape=(40,40,40,40), compression='gzip', compression_opts=6, shuffle=True, dtype=dt, data=arr4)
+# initialize dataset values here
+f.close()
+
+
