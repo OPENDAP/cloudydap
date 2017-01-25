23b43d9c870d8de3c8e559bdf4fe7277bd71f6de
H. Joe Lee <hyoklee@hdfgroup.org>
2017-01-25 13:43:44 -0600

    EED-177:added cloudydap parameter for dap4 test cases.

--- shell/use-cases/test_2_arch2.sh
+++ shell/use-cases/test_2_arch2.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Copyright (C) 2016 The HDF Group
+# Copyright (C) 2016-2017 The HDF Group
 # All rights reserved.
 #
 # https://github.com/OPENDAP/cloudydap/wiki/Trade-Study-Use-Case-2---DAP4-with-CF
 #
+CLOUDYDAP_TAG="UC2_A2CFT_STARTED_"`date +%s`
 echo 'Test AIRS DMR.'
-time curl -s http://a2cft.opendap.org:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dmr -o airs.dmr
+time curl -s http://a2cft.opendap.org:8080/opendap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dmr -o airs.dmr
 
 echo 'Test AIRS DAP4.'
-time curl -s http://a2cft.opendap.org:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Latitude\;Longitude\;Topography -o airs_d4_ll.bin
+time curl -s http://a2cft.opendap.org:8080/opendap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Latitude\;Longitude\;Topography\&cloudydap=$CLOUDYDAP_TAG -o airs_d4_ll.bin
 
 echo 'Test AIRS DAP4 subsetting.'
-time curl -s -g http://a2cft.opendap.org:8080/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\] -o airs_d4_ss.bin
+time curl -s -g http://a2cft.opendap.org:8080/opendap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\]\&cloudydap=$CLOUDYDAP_TAG -o airs_d4_ss.bin
 
 # echo 'Test AIRS DMR (local).'
 # time curl -s http://localhost:8081/opendap/s3/dap/cloudydap/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dmr -o airs_local.dmr
