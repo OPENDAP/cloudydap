897cd5b8f6ffcdb8ea72aa4e20df8986b8dea179
H. Joe Lee <hyoklee@hdfgroup.org>
2017-02-06 16:52:53 -0600

    EED-183:added arch3 tests.

---
+++ shell/use-cases/test_6_arch3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (C) 2017 The HDF Group
+# All rights reserved.
+#
+# MERRA aggregation test.
+# Run it on AWS EC2 instance where Hyrax server is running.
+#
+# https://github.com/OPENDAP/cloudydap/wiki/Trade-Study-Use-Case-6-----Simulate-NcML-JoinExisting-aggregation
+
+# Set path and environment variable.
+cd ~/hyrax
+. spath.sh
+
+# Run aggregation script.
+cd ~/hyrax/cloudydap/shell/aggEmulator
+rm -rf arch3/merra2
+rm -f /tmp/getDAP_*
+date >> test_6.log
+./arch3_aggTestMerra2 UC6
+date >> test_6.log
+diff -r arch3/merra2 ../use-cases/merra2
---
+++ shell/use-cases/test_7_arch3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (C) 2017 The HDF Group
+# All rights reserved.
+#
+# AIRS aggregation test.
+# https://github.com/OPENDAP/cloudydap/wiki/Trady-Study-Use-Case-7----Simulate-NcML-JoinNew-aggreegation
+
+# Set path and environment variable.
+cd ~/hyrax
+. spath.sh
+
+# Run aggregation script.
+cd ~/hyrax/cloudydap/shell/aggEmulator
+rm -rf arch3/airs
+rm -f /tmp/getDAP_*
+date >> test_7.log
+./arch3_aggTestAirs UC7
+date >> test_7.log
+diff -r arch3/airs ../use-cases/airs
