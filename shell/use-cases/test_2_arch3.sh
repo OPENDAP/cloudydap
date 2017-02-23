cae144745649571dcf53e62911d67a4509e56d84
ndp-opendap <ndp@opendap.org>
2017-02-22 17:51:37 -0800

    Repaired problem test_2_arch3 in which the cloudydap query parameter was not cloudydap but rather arch3. Changed to cloudydap

--- shell/use-cases/test_2_arch3.sh
+++ shell/use-cases/test_2_arch3.sh
@@ -10,10 +10,10 @@ echo 'Test AIRS DMR.'
 time curl -s http://cloudydap.opendap.org:8080/opendap/arch3/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dmr -o airs.dmr
 
 echo 'Test AIRS DAP4.'
-time curl -s http://cloudydap.opendap.org:8080/opendap/arch3/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Latitude\;Longitude\;Topography\&arch3=$CLOUDYDAP_TAG -o airs_d4_ll.bin
+time curl -s http://cloudydap.opendap.org:8080/opendap/arch3/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Latitude\;Longitude\;Topography\&cloudydap=$CLOUDYDAP_TAG -o airs_d4_ll.bin
 
 echo 'Test AIRS DAP4 subsetting.'
-time curl -s -g http://cloudydap.opendap.org:8080/opendap/arch3/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\]\&arch3=$CLOUDYDAP_TAG -o airs_d4_ss.bin
+time curl -s -g http://cloudydap.opendap.org:8080/opendap/arch3/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dap\?dap4.ce=Topography\[0:20:179\]\[0:20:359\]\&cloudydap=$CLOUDYDAP_TAG -o airs_d4_ss.bin
 
 # echo 'Test AIRS DMR (local).'
 # time curl -s http://localhost:8081/opendap/s3/dap/arch3/airs/AIRS.2015.01.01.L3.RetStd_IR001.v6.0.11.0.G15013155825.nc.h5.dmrpp.dmr -o airs_local.dmr
