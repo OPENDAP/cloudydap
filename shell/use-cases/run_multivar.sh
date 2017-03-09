631076760ab1e76d08e44e919c97cd06b2000dda
Nathan Potter <ndp@opendap.org>
2017-03-09 10:08:09 -0800

    sync

--- shell/use-cases/run_multivar.sh
+++ shell/use-cases/run_multivar.sh
@@ -7,15 +7,15 @@ for i in 17 18 19 20 21
 do
     echo "";
     echo "Test $i A1"
-    time -p { "test_"$i".sh"       > log/"test_"$i"_arch1.log" 2>&1 ;}
+    time -p { "test_"$i".sh"       > logs/"test_"$i"_arch1.log" 2>&1 ;}
     
     echo "";
     echo "Test $i A2"
-    time -p { "test_"$i"_arch2.sh" > log/"test_"$i"_arch2.log" 2>&1 ;}
+    time -p { "test_"$i"_arch2.sh" > logs/"test_"$i"_arch2.log" 2>&1 ;}
     
     echo "";
     echo "Test $i A3"
-    time -p { "test_"$i"_arch3.sh" > log/"test_"$i"_arch3.log" 2>&1 ;}
+    time -p { "test_"$i"_arch3.sh" > logs/"test_"$i"_arch3.log" 2>&1 ;}
 
 done
 
