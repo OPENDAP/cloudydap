540ceb1b181bd3be12607fc7d28a49a123c5a9ab
ndp-opendap <ndp@opendap.org>
2017-03-11 14:58:23 -0800

    bug

--- shell/use-cases/stats.sh
+++ shell/use-cases/stats.sh
@@ -6,21 +6,21 @@ mean_stdev()
     echo "$1" |
         awk '
         {
-            print "Value: "$1;
+            #print "Value: "$1;
             if(NR==1){
                 max=$1;
                 min=$1;
-                print "initialized max: "max" min: "min;
+                #print "initialized max: "max" min: "min;
             }
             sum+=$1; 
             sumsq+=$1*$1
             if(max<$1){
                 max=$1;
-                print "********** Changed max to "max;
+                #print "********** Changed max to "max;
             }
             if(min>$1){
                 min=$1;
-                print "********** Changed min to "min;
+                #print "********** Changed min to "min;
             }
         }
         END{ 
@@ -34,7 +34,7 @@ mean_stdev()
 for file in "$@"
 do
     vals=`grep real $file | awk '{print $2}' -`;
-    mean_stdev "$vals";    
-    #echo  $file" "$( mean_stdev "$vals" );    
+    #mean_stdev "$vals";    
+    echo  $file" "$( mean_stdev "$vals" );    
 
 done
